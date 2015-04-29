9f55bc07c0e5d22093b255628a6590ce324108a1
power <power0721@gmail.com>
2015-04-29 23:26:24 +0800

    fix RF table

--- src/syscalls.h
+++ src/syscalls.h
@@ -34,7 +34,30 @@ static int syscalls[1024] = {0};
 // http://docs.cs.up.ac.za/programming/asm/derick_tut/syscalls.html
 // http://syscalls.kernelgrok.com/
 
-// C or C++
+// C
+static int SC_C[512] =
+{
+  252,                  2,
+  SYS_access,           SYSCALLS_INFINITE,
+  SYS_brk,              SYSCALLS_INFINITE,
+  SYS_close,            SYSCALLS_INFINITE,
+  SYS_execve,           1,
+  SYS_fstat64,          SYSCALLS_INFINITE,
+  SYS_mmap2,            SYSCALLS_INFINITE,
+  SYS_mprotect,         SYSCALLS_INFINITE,
+  SYS_munmap,           SYSCALLS_INFINITE,
+  SYS_read,             SYSCALLS_INFINITE,
+  SYS_readlink,         SYSCALLS_INFINITE,
+  SYS_rt_sigaction,     SYSCALLS_INFINITE,  // Ubuntu
+  SYS_rt_sigprocmask,   SYSCALLS_INFINITE,  // Ubuntu
+  SYS_set_thread_area,  SYSCALLS_INFINITE,
+  SYS_time,             SYSCALLS_INFINITE,
+  SYS_uname,            SYSCALLS_INFINITE,
+  SYS_write,            SYSCALLS_INFINITE,
+  SYSCALLS_END
+};
+
+// C++
 static int SC_C[512] =
 {
   252,                  2,
@@ -170,26 +193,65 @@ static int SC_PYTHON[512] = {
 #else
 // http://blog.rchapman.org/post/36801038863/linux-system-call-table-for-x86-64
 
-// C or C++
+// C
 static int SC_C[512] =
 {
   252,                 2,
-  SYS_access,          SYSCALLS_INFINITE,
-  SYS_arch_prctl,      SYSCALLS_INFINITE,
+  SYS_access,          1,
+  SYS_arch_prctl,      1,
   SYS_brk,             SYSCALLS_INFINITE,
-  SYS_close,           SYSCALLS_INFINITE,
-  SYS_execve,          SYSCALLS_INFINITE,
-  SYS_exit_group,      SYSCALLS_INFINITE,
-  SYS_fstat,           SYSCALLS_INFINITE,
+  SYS_close,           1,
+  SYS_clock_gettime,   SYSCALLS_INFINITE,
+  SYS_execve,          1,
+  SYS_exit_group,      1,
+  SYS_fstat,           3,
+  SYS_get_thread_area, 1,
+  SYS_gettid,          1,
+  SYS_open,            1,
   SYS_mmap,            SYSCALLS_INFINITE,
-  SYS_mprotect,        SYSCALLS_INFINITE,
+  SYS_mprotect,        16,
   SYS_munmap,          SYSCALLS_INFINITE,
   SYS_read,            SYSCALLS_INFINITE,
-  SYS_readlink,        SYSCALLS_INFINITE,
-  SYS_set_thread_area, SYSCALLS_INFINITE,
+  SYS_readlink,        1,
+  SYS_rt_sigprocmask,  1,
+  SYS_set_thread_area, 1,
+  SYS_tgkill,          2,
   SYS_time,            SYSCALLS_INFINITE,
-  SYS_uname,           SYSCALLS_INFINITE,
+  SYS_uname,           1,
   SYS_write,           SYSCALLS_INFINITE,
+  SYS_writev,          SYSCALLS_INFINITE,
+  SYSCALLS_END
+};
+
+// C++
+static int SC_CPP[512] =
+{
+  252,                 2,
+  SYS_access,          1,
+  SYS_arch_prctl,      1,
+  SYS_brk,             SYSCALLS_INFINITE,
+  SYS_close,           1,
+  SYS_clock_gettime,   1,
+  SYS_execve,          1,
+  SYS_exit_group,      1,
+  SYS_fstat,           3,
+  SYS_futex,           SYSCALLS_INFINITE,
+  SYS_get_thread_area, 1,
+  SYS_gettid,          1,
+  SYS_mprotect,        16,
+  SYS_mmap,            SYSCALLS_INFINITE,
+  SYS_munmap,          SYSCALLS_INFINITE,
+  SYS_open,            2,
+  SYS_read,            SYSCALLS_INFINITE,
+  SYS_readlink,        1,
+  SYS_rt_sigprocmask,  1,
+  SYS_set_thread_area, 1,
+  SYS_set_tid_address, 1,
+  SYS_tgkill,          1,
+  SYS_time,            SYSCALLS_INFINITE,
+  SYS_uname,           1,
+  SYS_write,           SYSCALLS_INFINITE,
+  SYS_writev,          SYSCALLS_INFINITE,
   SYSCALLS_END
 };
 
@@ -226,47 +288,48 @@ static int SC_JAVA[512] =
   110,                 SYSCALLS_INFINITE,
   111,                 SYSCALLS_INFINITE,
   13,                  SYSCALLS_INFINITE,
-  158,                 SYSCALLS_INFINITE,
+  SYS_arch_prctl,      1,
   16,                  SYSCALLS_INFINITE,
   22,                  SYSCALLS_INFINITE,
-  257,                 SYSCALLS_INFINITE,
   33,                  SYSCALLS_INFINITE,
   39,                  SYSCALLS_INFINITE,
   6,                   SYSCALLS_INFINITE,
   61,                  SYSCALLS_INFINITE,
   79,                  SYSCALLS_INFINITE,
   8,                   SYSCALLS_INFINITE,
-  SYS_access,          SYSCALLS_INFINITE,
-  SYS_brk,             SYSCALLS_INFINITE,
-  SYS_clone,           SYSCALLS_INFINITE,
+  SYS_access,          16,
+  SYS_brk,             4,
+  SYS_clock_gettime,   SYSCALLS_INFINITE,
+  SYS_clone,           1,
   SYS_close,           SYSCALLS_INFINITE,
-  SYS_close,           SYSCALLS_INFINITE,
-  SYS_execve,          SYSCALLS_INFINITE,
-  SYS_exit_group,      SYSCALLS_INFINITE,
+  SYS_execve,          1,
+  SYS_exit_group,      1,
+  SYS_get_thread_area, 1,
   SYS_fcntl,           SYSCALLS_INFINITE,
   SYS_fstat,           SYSCALLS_INFINITE,
   SYS_futex,           SYSCALLS_INFINITE,
-  SYS_getdents64,      SYSCALLS_INFINITE,
+  SYS_getdents64,      2,
   SYS_getegid,         SYSCALLS_INFINITE,
   SYS_geteuid,         SYSCALLS_INFINITE,
   SYS_getgid,          SYSCALLS_INFINITE,
-  SYS_getrlimit,       SYSCALLS_INFINITE,
-  SYS_getrlimit,       SYSCALLS_INFINITE,
+  SYS_getrlimit,       1,
+  SYS_openat,          1,
   SYS_getuid,          SYSCALLS_INFINITE,
   SYS_mmap,            SYSCALLS_INFINITE,
   SYS_mprotect,        SYSCALLS_INFINITE,
   SYS_munmap,          SYSCALLS_INFINITE,
   SYS_open,            SYSCALLS_INFINITE,
   SYS_read,            SYSCALLS_INFINITE,
-  SYS_readlink,        SYSCALLS_INFINITE,
-  SYS_rt_sigaction,    SYSCALLS_INFINITE,
-  SYS_rt_sigprocmask,  SYSCALLS_INFINITE,
-  SYS_set_robust_list, SYSCALLS_INFINITE,
-  SYS_set_thread_area, SYSCALLS_INFINITE,
-  SYS_set_tid_address, SYSCALLS_INFINITE,
+  SYS_readlink,        2,
+  SYS_rt_sigaction,    2,
+  SYS_rt_sigprocmask,  1,
+  SYS_set_robust_list, 1,
+  SYS_set_thread_area, 1,
+  SYS_set_tid_address, 1,
+  SYS_time,            SYSCALLS_INFINITE,
   SYS_stat,            SYSCALLS_INFINITE,
-  SYS_uname,           SYSCALLS_INFINITE,
-  SYS_uname,           SYSCALLS_INFINITE,
+  SYS_uname,           1,
+  SYS_write,           SYSCALLS_INFINITE,
   SYSCALLS_END
 };
 
@@ -324,8 +387,10 @@ int init_syscalls(int lang)
   int *p = NULL;
   switch (lang) {
     case LANG_C:
-    case LANG_CPP:
       p = SC_C;
+      break;
+    case LANG_CPP:
+      p = SC_CPP;
       break;
     case LANG_PASCAL:
       p = SC_PASCAL;
